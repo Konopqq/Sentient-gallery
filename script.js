34295332c1cf6eccff87821a882f72e39f44dcc2
Konopqq <konopqgg@gmail.com>
2025-10-11 17:41:33 +0300

    update

--- script.js
+++ script.js
@@ -74,7 +74,14 @@ document.addEventListener('DOMContentLoaded', () => {
       download.className = 'download-btn';
       download.href = photo.file;
       download.setAttribute('download', '');
-      download.textContent = '⬇️ Скачать';
+download.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+    <line x1="12" y1="5" x2="12" y2="17"></line>
+    <polyline points="5 12 12 19 19 12"></polyline>
+  </svg>
+`;
+
+
       buttons.appendChild(download);
 
       card.appendChild(title);
